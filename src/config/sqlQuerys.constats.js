f10bbeb416b15d459d998d610fbb49b6781b23a1
Informativus <ivanpopov123321123@gmail.com>
2024-09-11 21:27:59 +0300

    Update sql constats

--- src/config/sqlQuerys.constats.js
+++ src/config/sqlQuerys.constats.js
@@ -1,7 +1,12 @@
-export const GET_PRODUCTS = "SELECT * FROM products";
-export const GET_PRODUCT = "SELECT * FROM products WHERE plu = $1";
-export const GET_PRODUCT_WITH_SHOP_ID =
-  "SELECT * FROM products WHERE plu = (SELECT plu FROM stores WHERE id = $1)";
+// PRODUCT
+export const GET_PRODUCTS = "SELECT * FROM Products";
+export const GET_PRODUCT = "SELECT * FROM Products WHERE plu = $1";
+export const GET_PRODUCT_PLU_WITH_SHOP_ID =
+  "SELECT * FROM Product_Store WHERE store_id = $1";
 // TODO подумать как создавать plu
 export const CREATE_PRODUCT =
-  "INSERT INTO products (name, price) VALUES ($1, $2)";
+  "INSERT INTO Products (plu, name, price) VALUES ($1, $2, $3)";
+
+// STORE
+export const GET_STORE_ID_BY_NAME = "SELECT id FROM Stores WHERE name = $1";
+export const CREATE_STORE = "INSERT INTO Stores (name) VALUES ($1)";
